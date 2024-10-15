6ef05717207ac56d9f67e2d3fc0aedc3b56fe0c9
Huyz0906 <huythce181180@fpt.edu.vn>
2024-10-15 18:35:41 +0700

    Hiển thị thông tin của Staff (chưa hoàn thành)

---
+++ TaskForge/Service/EmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using TaskForge.Models;
+using TaskForge.Repository;
+
+namespace TaskForge.Service
+{
+    public class EmployeeService
+    {
+        private readonly EmployeeRepository _employeeRepository;
+
+        public EmployeeService(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public Employee GetEmployeeByAccountId(string accountId)
+        {
+            return _employeeRepository.GetEmployeeByAccountId(accountId);
+        }
+    }
+}
